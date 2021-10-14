9164a99c99b297904c2cf8bb3b47d0f676c73842
Vidhanth Bafna <vidhanthbafna@gmail.com>
2021-10-14 09:54:10 +0530

    Adjust light theme colors

--- lib/controllers/ThemeController.dart
+++ lib/controllers/ThemeController.dart
@@ -17,13 +17,13 @@ class ThemeController extends GetxController {
   static Function exitWipe = () {};
 
   static ThemeColors lightTheme = ThemeColors(
-    backgroundColor: Color(0xFFF0F0F0),
+    backgroundColor: Color(0xFFE9E9E9),
     foreground: Colors.black.withOpacity(0.9),
     accent: Colors.black,
     focusColor: Colors.black.withOpacity(0.05),
     hoverColor: Colors.black.withOpacity(0.05),
     splashColor: Colors.black12,
-    mutedBg: Color(0xFFDFDFDF),
+    mutedBg: Color(0xFFD3D3D3),
     subtext: Colors.grey.shade700,
     contrastText: Colors.white.withOpacity(0.9),
   );
