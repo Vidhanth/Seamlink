5a49dd7d9481d128bc45d6d0f3cdd9988ffa2542
Vidhanth <vidhanthbafna@gmail.com>
2022-07-02 17:50:16 +0530

    Don't run any PlatformMenuBar related code on non Mac devices

---
+++ lib/components/menubar.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:seamlink/controllers/HomeController.dart';
+import 'package:seamlink/controllers/ThemeController.dart';
+import 'package:seamlink/controllers/UserController.dart';
+
+import '../services/utils.dart';
+
+class MenuBar extends StatelessWidget {
+  final Widget child;
+  final bool enable;
+
+  const MenuBar({Key? key, required this.child, required this.enable})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (enable)
+      return Obx(() {
+        bool isLoggedIn = Get.find<UserController>().username.isNotEmpty;
+
+        return PlatformMenuBar(
+          menus: <MenuItem>[
+            PlatformMenu(
+              label: 'Seamlink',
+              menus: <MenuItem>[
+                if (isLoggedIn) ...Get.find<HomeController>().getMenuItems(),
+                PlatformMenuItemGroup(
+                  members: [
+                    PlatformMenuItem(
+                      label: ThemeController.isAuto
+                          ? 'Light Mode'
+                          : ThemeController.isDark
+                              ? 'System Theme'
+                              : 'Dark Mode',
+                      onSelected: () async {
+                        await Get.find<ThemeController>().switchTheme();
+                      },
+                      shortcut: const SingleActivator(
+                        LogicalKeyboardKey.keyT,
+                        meta: true,
+                      ),
+                    ),
+                  ],
+                ),
+                if (isLoggedIn)
+                  PlatformMenuItemGroup(
+                    members: [
+                      PlatformMenuItem(
+                        label: 'Log Out',
+                        onSelected: () async {
+                          await logout();
+                        },
+                        shortcut: const SingleActivator(
+                          LogicalKeyboardKey.keyL,
+                          meta: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                if (PlatformProvidedMenuItem.hasMenu(
+                    PlatformProvidedMenuItemType.servicesSubmenu))
+                  const PlatformMenuItemGroup(
+                    members: [
+                      PlatformProvidedMenuItem(
+                        type: PlatformProvidedMenuItemType.servicesSubmenu,
+                      )
+                    ],
+                  ),
+                if (PlatformProvidedMenuItem.hasMenu(
+                    PlatformProvidedMenuItemType.quit))
+                  const PlatformProvidedMenuItem(
+                      type: PlatformProvidedMenuItemType.quit),
+              ],
+            ),
+          ],
+          body: child,
+        );
+      });
+    return child;
+  }
+}
